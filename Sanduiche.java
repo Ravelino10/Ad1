73a629e0e87461830674661c72f50fa298335b8f
Ravelino10 <ruan_avelino@hotmail.com>
2015-08-20 09:45:40 -0300

    Rename Lanche.java to Sanduiche.java

---
+++ Sanduiche.java
@@ -0,0 +1,10 @@
+class Sanduiche extends Lanche{
+
+	Sanduiche(String Ingrediente1, double preco1, String Ingrediente2, double preco2, String Ingrediente3, double preco3, String Ingrediente4, double preco4){
+		super(Ingrediente1, preco1, Ingrediente2, preco2, Ingrediente3, preco3, Ingrediente4, preco4);
+	}
+	
+	double getPrecoTotal() {
+		return = (preco1 + preco2 + preco3 + preco4) * 1,3;
+	}	
+}
