e547ba2b9a04f23ab15d75131c412a3b0a1c0dad
Ravelino10 <ruan_avelino@hotmail.com>
2015-08-20 18:12:31 -0300

    Create Pizza.java

---
+++ Pizza.java
@@ -0,0 +1,11 @@
+class Pizza extends Lanche{
+
+	Pizza(Carne Ingrediente1, Queijo Ingrediente2, Salada Ingrediente3, Molho Ingrediente4) {
+		super(Ingrediente1, Ingrediente2, Ingrediente3, Ingrediente4);
+	}
+	
+	double getPrecoTotal() {
+		return (Ingrediente1.preco + Ingrediente2.preco + Ingrediente3.preco + Ingrediente4.preco) * 1.4;
+	}	
+	
+}
