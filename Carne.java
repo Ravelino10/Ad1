a9f22c6d37df661ac495dcbadc28468b01133933
Ravelino10 <ruan_avelino@hotmail.com>
2015-08-20 18:15:00 -0300

    Update Carne.java

--- Carne.java
+++ Carne.java
@@ -3,15 +3,5 @@ class Carne extends Tipo{
 	Carne(String nome, double preco) {
 		super(nome, preco);
 	}
-		
-	public static void main (String arg[]) {
-		
-		Carne carne1, carne2, carne3;
-		
-		carne1 = new Carne ("Boi", 1.0);
-		carne2 = new Carne ("Peixe", 2.0);
-		carne3 = new Carne ("Frango", 1.5);
-		
-	}
 	
 }
