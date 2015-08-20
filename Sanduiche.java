5bcd6a3c248ed66487e8887648172b07240e5042
Ravelino10 <ruan_avelino@hotmail.com>
2015-08-20 18:13:08 -0300

    Update Sanduiche.java

--- Sanduiche.java
+++ Sanduiche.java
@@ -1,10 +1,11 @@
 class Sanduiche extends Lanche{
 
-	Sanduiche(String Ingrediente1, double preco1, String Ingrediente2, double preco2, String Ingrediente3, double preco3, String Ingrediente4, double preco4){
-		super(Ingrediente1, preco1, Ingrediente2, preco2, Ingrediente3, preco3, Ingrediente4, preco4);
+	Sanduiche(Carne Ingrediente1, Queijo Ingrediente2, Salada Ingrediente3, Molho Ingrediente4) {
+		super(Ingrediente1, Ingrediente2, Ingrediente3, Ingrediente4);
 	}
 	
 	double getPrecoTotal() {
-		return = (preco1 + preco2 + preco3 + preco4) * 1,3;
+		return (Ingrediente1.preco + Ingrediente2.preco + Ingrediente3.preco + Ingrediente4.preco) * 1.3;
 	}	
+	
 }
